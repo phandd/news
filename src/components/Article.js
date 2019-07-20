d27293ceef1dddf78fe561a047487de5520804ec
Dang Dien PHAN <ddphan@linagora.com>
2019-07-20 15:32:49 +0700

    articles list view

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import './Article.less'
 
 const Article = ({ article }) => {
-  const { title, source, url, urlToImage, desciption } = article
+  const { title, source, url, urlToImage, description } = article
 
   return (
-    <div>{title}</div>
+    <div className="article-wrapper">
+      <div className="row">
+        <div className="col-md-10 article">
+          <div className="col-md-2 article-image">
+            <img src={urlToImage} alt={title} className="img-responsive"></img>
+          </div>
+          <div className="col-md-10">
+            <a href={url} target="_blank" className="article-data">
+              <h4>{title}</h4>
+              <p className="article-description">{description}</p>
+              <span className="article-source">{source.name}</span>
+            </ a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './index.less'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
